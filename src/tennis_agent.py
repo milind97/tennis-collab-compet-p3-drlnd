612c15cec51c87cfc810b53c7ddec1a759d85f80
_milindmahajan <milind.mahajan@natwest.com>
2022-06-29 15:45:41 +0530

    Fixed import error

--- src/tennis_agent.py
+++ src/tennis_agent.py
@@ -3,7 +3,7 @@ import random
 import copy
 from collections import namedtuple, deque
 
-from tennis_model import Actor, Critic
+from src.tennis_model import Actor, Critic
 
 import torch
 import torch.nn.functional as F
